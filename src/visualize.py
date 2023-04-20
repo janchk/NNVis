d2e2c88fd6be76f8d91388180373f7077f8ceb10
janchk <janchk@gmail.com>
2023-04-20 17:10:18 +0300

    Add names to plots

--- src/visualize.py
+++ src/visualize.py
@@ -25,7 +25,7 @@ class NVIS():
     def set_model(self, model: torch.nn.Module) -> None:
         self.model = model
 
-    def plot_weights_distributions(self):
+    def plot_weights_distributions(self, name=""):
         self.plots = []
         def filter(name): return name.find(".bn") == -1 and "bias" not in name
 
@@ -34,9 +34,9 @@ class NVIS():
                 plot = self.plotter.layer_violin_plot(name, weights)
                 self.plots.append(plot)
 
-        pdf_plot(self.plots, "weights")
+        pdf_plot(self.plots, f"weights_{name}")
 
-    def plot_activations_distributions(self, input_shape):
+    def plot_activations_distributions(self, input_shape, name=""):
         self.plots = []
         data = torch.rand(input_shape)
         self.hook = hooks.OutputHook()
@@ -50,7 +50,7 @@ class NVIS():
 
         hook_unregister(handles)
 
-        pdf_plot(self.plots, "activations")
+        pdf_plot(self.plots, f"activations_{name}")
 
     # def export_pdf(self):
         # plotter = self.plotter
